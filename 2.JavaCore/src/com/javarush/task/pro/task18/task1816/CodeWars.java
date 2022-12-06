366f46a4b9262a46bae70ad6dd8525f4345f0f3a
Aleksei Briukhachev <abriukhachev@trask.cz>
2022-12-06 09:37:04 +0100

    update

--- 2.JavaCore/src/com/javarush/task/pro/task18/task1816/CodeWars.java
+++ 2.JavaCore/src/com/javarush/task/pro/task18/task1816/CodeWars.java
@@ -11,31 +11,31 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class CodeWars {
-    @Test
-    public void sampleTests() {
-        assertArrayEquals(new int[] {2}, Kata.arrayDiff(new int [] {1,2}, new int[] {1}));
-        assertArrayEquals(new int[] {2,2}, Kata.arrayDiff(new int [] {1,2,2}, new int[] {1}));
-        assertArrayEquals(new int[] {1}, Kata.arrayDiff(new int [] {1,2,2}, new int[] {2}));
-        assertArrayEquals(new int[] {1,2,2}, Kata.arrayDiff(new int [] {1,2,2}, new int[] {}));
-        assertArrayEquals(new int[] {}, Kata.arrayDiff(new int [] {}, new int[] {1,2}));
-    }
-    public class Kata {
-
-        public static int[] arrayDiff(int[] a, int[] b) {
-            // Your code here
-            List<Integer> list = new ArrayList<>();
-            for (int ela : a){
-                for (int elb : b){
-                    if (ela != elb)
-                        list.add(elb);
-                }
-            }
-            int[] c = new int[list.size()];
-
-            return c;
-        }
-
-    }
+//    @Test
+//    public void sampleTests() {
+//        assertArrayEquals(new int[] {2}, Kata.arrayDiff(new int [] {1,2}, new int[] {1}));
+//        assertArrayEquals(new int[] {2,2}, Kata.arrayDiff(new int [] {1,2,2}, new int[] {1}));
+//        assertArrayEquals(new int[] {1}, Kata.arrayDiff(new int [] {1,2,2}, new int[] {2}));
+//        assertArrayEquals(new int[] {1,2,2}, Kata.arrayDiff(new int [] {1,2,2}, new int[] {}));
+//        assertArrayEquals(new int[] {}, Kata.arrayDiff(new int [] {}, new int[] {1,2}));
+//    }
+//    public class Kata {
+//
+//        public static int[] arrayDiff(int[] a, int[] b) {
+//            // Your code here
+//            List<Integer> list = new ArrayList<>();
+//            for (int ela : a){
+//                for (int elb : b){
+//                    if (ela != elb)
+//                        list.add(elb);
+//                }
+//            }
+//            int[] c = new int[list.size()];
+//
+//            return c;
+//        }
+//
+//    }
 
 }
 
